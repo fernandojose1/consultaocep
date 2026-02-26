0019b76ef342219a5f8f39d759171fe043514c1c
Fernando José <69250217+fernandojose1@users.noreply.github.com>
2026-02-26 11:59:32 -0300

    Melhora validação de CEP, segurança de saída e responsividade

--- consultaocep.php
+++ consultaocep.php
@@ -1,13 +1,50 @@
-<?php 
+<?php
 
 /* Desenvolvido por Fernando José
-    Aplicação com APi VIA CEP   */
+   Aplicação com API VIA CEP */
 
-    $cep = $_POST['cep'];
-    $urlApi = "https://viacep.com.br/ws/".$cep."/json/";
+$endereco = (object) [
+    'cep' => '',
+    'logradouro' => '',
+    'complemento' => '',
+    'bairro' => '',
+    'localidade' => '',
+    'uf' => '',
+    'ibge' => '',
+    'gia' => '',
+    'ddd' => '',
+    'siafi' => '',
+];
 
-    $endereco = json_decode(file_get_contents($urlApi),false);
+$erroConsulta = '';
 
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $cepInformado = $_POST['cep'] ?? '';
+    $cep = preg_replace('/\D/', '', $cepInformado);
 
+    if (strlen($cep) !== 8) {
+        $erroConsulta = 'CEP inválido. Digite 8 números.';
+    } else {
+        $urlApi = "https://viacep.com.br/ws/{$cep}/json/";
+        $contexto = stream_context_create([
+            'http' => [
+                'method' => 'GET',
+                'timeout' => 5,
+            ],
+        ]);
 
- ?>
+        $respostaApi = @file_get_contents($urlApi, false, $contexto);
+
+        if ($respostaApi === false) {
+            $erroConsulta = 'Não foi possível consultar o CEP no momento.';
+        } else {
+            $dadosApi = json_decode($respostaApi, false);
+
+            if (json_last_error() !== JSON_ERROR_NONE || ($dadosApi->erro ?? false) === true) {
+                $erroConsulta = 'CEP não encontrado.';
+            } else {
+                $endereco = (object) array_merge((array) $endereco, (array) $dadosApi);
+            }
+        }
+    }
+}
